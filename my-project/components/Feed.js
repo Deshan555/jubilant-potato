dab900d51647622bcceeb0a1e522da1e865f2e85
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-10-22 20:55:31 +0530

    Update feed.js
    make scope and make scope into comments

---
+++ my-project/components/Feed.js
@@ -0,0 +1,18 @@
+function Feed()
+{
+    {/* 1. Section -> Stories and Posts*/}
+
+    {/*2. Section -> Mini Profiles and Suggestions*/}
+
+    return(
+
+        <div>
+
+        
+
+        </div>
+    )
+
+}
+
+export default Feed
