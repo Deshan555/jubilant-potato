372322a7bce96a33944cd218dbae9c8193d7e078
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-10-22 21:39:17 +0530

    Update files and linking Stories to Feed.js

--- my-project/components/Feed.js
+++ my-project/components/Feed.js
@@ -1,16 +1,26 @@
+import Stories from "./Stories"
+
 function Feed()
 {
-    {/* 1. Section -> Stories and Posts*/}
-
-    {/*2. Section -> Mini Profiles and Suggestions*/}
-
     return(
 
-        <div>
+        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
 
+            {/* 1. Section -> Stories and Posts*/}
+
+            <section className="col-span-2">
+
+                <Stories/>
+
+            </section>
+
+            {/*2. Section -> Mini Profiles and Suggestions*/}
+
+            <section>
+
+            </section>
         
-
-        </div>
+        </main>
     )
 
 }
---
+++ my-project/components/Stories.js
@@ -0,0 +1,17 @@
+import faker from "faker"
+import { useEffect } from "react"
+
+
+function Stories()
+{
+
+    
+    return(
+
+        <div>
+
+        </div>
+    )
+}
+
+export default Stories
